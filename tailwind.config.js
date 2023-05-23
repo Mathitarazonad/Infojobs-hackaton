ad6af522f51f635c386a7ed7b5a092a31bb3f087
MathiTarazonad <vladitarazona@gmail.com>
2023-05-22 21:52:17 -0400

    Added a font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      }
     },
   },
   plugins: [],
