9012e96f920a818478ce19d3622c7926411d2a35
MathiTarazonad <vladitarazona@gmail.com>
2023-05-23 19:07:54 -0400

    Desactivated autocomplete

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,9 +1,9 @@
-export default function Search() {
+export default function Search () {
   return (
     <form className='flex flex-col gap-2'>
       <label className='text-white text-3xl md:text-4xl text-center font-medium' htmlFor='search'>Search for something</label>
       <div className='flex gap-3 justify-center'>
-        <input type='text' placeholder='e.g. architect' id='search' className='bg-transparent text-white placeholder:text-white border-2 border-white p-2 outline-none w-full' />
+        <input type='text' placeholder='e.g. architect' id='search' autoComplete='off' className='bg-transparent text-white placeholder:text-white border-2 border-white p-2 outline-none w-full' />
         <button type='submit' className='text-white p-2 border-2 border-white hover:bg-white hover:bg-opacity-30 duration-200'>Search</button>
       </div>
     </form>
