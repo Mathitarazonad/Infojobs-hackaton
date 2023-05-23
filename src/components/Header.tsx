1a58067a61ff14c58ac2103c7150dda4c2389322
MathiTarazonad <vladitarazona@gmail.com>
2023-05-23 18:58:11 -0400

    Changed img element for the Image next component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import LogInButton from './LogInButton'
 
-export default function Header() {
+export default function Header () {
   return (
     <header className='w-screen bg-sky-600 absolute flex justify-between items-center py-3 p-4 sm:px-[50px] lg:px-[200px]'>
-      <img src='/images/infojobs.svg' alt='Infojobs Logo' className='w-[100px]'/>
+      <Image src='/images/infojobs.svg' alt='Infojobs Logo' width={100} height={100} />
       <LogInButton />
     </header>
   )
