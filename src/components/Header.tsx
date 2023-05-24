3c86abc8e5f42cb65cb96562a683c57fe0a020a8
MathiTarazonad <vladitarazona@gmail.com>
2023-05-24 09:32:30 -0400

    Deleted width that was making to add an unexpected x-scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import LogInButton from './LogInButton'
 
 export default function Header () {
   return (
-    <header className='w-screen bg-sky-600 absolute flex justify-between items-center py-3 p-4 sm:px-[50px] lg:px-[200px]'>
+    <header className='w-full bg-sky-600 absolute flex justify-between items-center py-3 p-4 sm:px-[50px] lg:px-[200px]'>
       <Image src='/images/infojobs.svg' alt='Infojobs Logo' width={100} height={100} />
       <LogInButton />
     </header>
