393c23247ba6d46ca0ae6224eb88b3e3ee7a2dfe
MathiTarazonad <vladitarazona@gmail.com>
2023-05-25 10:15:42 -0400

    Moved some types to general type declaration file

--- src/app/types/filterTypes.ts
+++ src/app/types/filterTypes.ts
@@ -1,14 +1,8 @@
+import { Contract, JobModality, Language, Workday } from './types'
+
 export interface EmployerFilters {
   modality: JobModality
   contract: Contract
   workday: Workday
   languages: Language[]
 }
-
-type JobModality = 'presential' | 'hybrid' | 'remote' | 'any'
-
-type Contract = 'full-time' | 'fixed-term' | 'any'
-
-type Workday = 'complete' | 'partial' | 'undefined'
-
-type Language = 'english' | 'spanish' | 'french' | 'german'
---
+++ src/app/types/types.d.ts
@@ -0,0 +1,7 @@
+export type JobModality = 'presential' | 'hybrid' | 'remote' | 'any'
+
+export type Contract = 'full-time' | 'fixed-term' | 'any'
+
+export type Workday = 'complete' | 'partial' | 'undefined'
+
+export type Language = 'english' | 'spanish' | 'french' | 'german'
