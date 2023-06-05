be01ee93f2f509024c4d56ef4ff1d3c2cdda6036
MathiTarazonad <vladitarazona@gmail.com>
2023-06-05 15:22:47 -0400

    Deleted react content loader and instead used lucide react skeleton, also changed loader card name

---
+++ src/components/card/CardLoader.tsx
@@ -0,0 +1,27 @@
+import { Skeleton } from '../shadcn/skeleton'
+
+export default function CardLoader () {
+  return (
+    <article className='drop-shadow-lg w-full min-w-[300px] min-h-[176px] md:min-h-[220px] bg-white p-2 md:p-4 lg:p-6 flex gap-2 rounded-lg'>
+      <Skeleton className='rounded-full w-[45px] min-w-[45px] h-[45px] md:w-[55px] md:min-w[55px] md:h-[55px] lg:w-[65px] lg:min-w-[65px] lg:h-[65px] bg-gray-300 object-cover' />
+      <div className='flex flex-col gap-2 md:gap-4 w-full'>
+        <div className='flex justify-between items-center'>
+          <section className='flex gap-2 md:gap-4'>
+            <Skeleton className='p-4 w-[100px] bg-gray-300 rounded-lg' />
+            <Skeleton className='p-4 w-[100px] bg-gray-300 rounded-lg' />
+          </section>
+        </div>
+        <Skeleton className='w-[100px] bg-gray-300 p-2' />
+        <Skeleton className='w-[100px] bg-gray-300 p-1' />
+        <div className='flex justify-start items-center gap-3'>
+          <Skeleton className='bg-gray-300 flex-1 md:min-w-[95px] p-1' />
+          <Skeleton className='bg-gray-300 flex-1 md:min-w-[95px] p-1' />
+          <Skeleton className='bg-gray-300 flex-1 md:min-w-[95px] p-1' />
+        </div>
+        <Skeleton className='bg-gray-300 w-full p-1' />
+        <Skeleton className='bg-gray-300 w-[80%] p-1' />
+        <Skeleton className='bg-gray-300 w-[70%] p-1' />
+      </div>
+    </article>
+  )
+}
--- src/components/card/JobSeekersList.tsx
+++ src/components/card/JobSeekersList.tsx
@@ -2,17 +2,15 @@
 import { JobSeeker } from '@/types/types'
 import JobSeekerCard from './JobSeekerCard'
 import { useDocuments } from '@/hooks/useDocuments'
-import MyLoader from './LoaderCard'
+import CardLoader from './CardLoader'
 
 export default function JobSeekersList () {
   const { documents: jobSeekerList, loadingDocuments } = useDocuments()
 
   if (loadingDocuments) {
     return (
-      <ul className='max-w-full flex flex-col gap-5'>
-        <MyLoader />
-        <MyLoader />
-        <MyLoader />
+      <ul className='w-full flex flex-col gap-5'>
+        {Array(6).map((e, i) => <CardLoader key={i} />)}
       </ul>
     )
   }
