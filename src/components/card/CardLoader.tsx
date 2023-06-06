9cda5bc2989d24c1b34e14c5d3d640d589b951e8
MathiTarazonad <vladitarazona@gmail.com>
2023-06-06 14:09:55 -0400

    Fixed some styles

--- src/components/card/CardLoader.tsx
+++ src/components/card/CardLoader.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '../shadcn/skeleton'
 export default function CardLoader () {
   return (
     <article className='drop-shadow-lg w-full min-w-[300px] min-h-[176px] md:min-h-[220px] bg-white p-2 md:p-4 lg:p-6 flex gap-2 rounded-lg'>
-      <Skeleton className='rounded-full w-[45px] min-w-[45px] h-[45px] md:w-[55px] md:min-w[55px] md:h-[55px] lg:w-[65px] lg:min-w-[65px] lg:h-[65px] bg-gray-300 object-cover' />
+      <Skeleton className='rounded-full w-[45px] min-w-[45px] h-[45px] md:w-[55px] md:min-w-[55px] md:h-[55px] lg:w-[65px] lg:min-w-[65px] lg:h-[65px] bg-gray-300 object-cover' />
       <div className='flex flex-col gap-2 md:gap-4 w-full'>
         <div className='flex justify-between items-center'>
           <section className='flex gap-2 md:gap-4'>
