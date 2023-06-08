669494a7a4919cea27046efaf0d9bd3c1da017b1
MathiTarazonad <vladitarazona@gmail.com>
2023-06-08 11:07:34 -0400

    Edited some eslint rules and the menu

--- src/components/header/MobileMenu.tsx
+++ src/components/header/MobileMenu.tsx
@@ -1,38 +1,59 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/promise-function-async */
 /* eslint-disable react/jsx-closing-tag-location */
 import { useAuth } from '@/hooks/useAuth'
 import { Employer, JobSeeker } from '@/types/types'
 import Image from 'next/image'
+import { BiLogOut } from 'react-icons/bi'
+import { BsBookmarksFill, BsCardChecklist } from 'react-icons/bs'
+import { FaUserCircle } from 'react-icons/fa'
 import { IoMdClose } from 'react-icons/io'
 
 export default function MobileMenu ({ isOpen, handleClick }: { isOpen: boolean, handleClick: () => void }) {
-  const { currentUser } = useAuth()
-  const { fullname, photoURL, userType } = currentUser as JobSeeker | Employer
+  const { currentUser, logout } = useAuth()
+  const { fullname, email, photoURL, userType } = currentUser as JobSeeker | Employer
 
   return (
-    <div className={(isOpen ? 'flex' : 'hidden') + ' flex-col gap-2 absolute top-0 right-0 md:right-[50px] md:top-[64px] xl:right-[200px] w-[300px] bg-white z-10 drop-shadow-lg text-black overflow-hidden'}>
+    <div className={(isOpen ? 'flex' : 'hidden') + ' flex-col gap-2 rounded-lg absolute top-0 right-0 md:right-[50px] md:top-[68px] xl:right-[200px] w-[300px] bg-white text-gray-900 z-10 drop-shadow-lg overflow-hidden'}>
       <section className='border-b-2 border-gray-300 flex items-center justify-between h-[68px] px-2'>
         <div className='flex items-center gap-2'>
-          <Image width={50} height={50} src={photoURL !== '' ? photoURL as string : 'images/defaultProfilePhoto.svg'} className='rounded-full w-[50px] min-w-[50px] h-[50px] bg-white object-cover' alt='alt' />
-          <h3 className='text-xl font-semibold'>{fullname}</h3>
+          <Image width={50} height={50} src={photoURL !== '' ? photoURL as string : 'images/defaultProfilePhoto.svg'} className='rounded-full w-[40px] min-w-[40px] h-[40px] bg-white object-cover' alt='alt' />
+          <div className='max-w-[210px]'>
+            <h3 className='text-xl font-semibold'>{fullname}</h3>
+            <p className='text-xs break-words'>{email}</p>
+          </div>
         </div>
-        <div onClick={() => handleClick()}>
+        <div onClick={() => handleClick()} className='hover:scale-110 duration-200'>
           <IoMdClose fontSize={24} />
         </div>
       </section>
-      <section className='flex flex-col gap-2 px-4 py-2 text-lg border-b-2 border-gray-300'>
-        <h2 className='text-xl font-semibold'>Profile</h2>
-        <p className='min-w-[84px] cursor-pointer md:hover:font-semibold duration-200'>My profile</p>
-        <p className='min-w-[86px] cursor-pointer md:hover:font-semibold duration-200'>Sign Out</p>
+      <section className='flex flex-col gap-2 pl-4 py-2 text-lg border-b-2 border-gray-300'>
+        <h3 className='text-lg font-semibold'>Profile</h3>
+        <div className='flex gap-2 items-center'>
+          <FaUserCircle />
+          <p onClick={() => logout()}>My profile</p>
+        </div>
+        <div className='flex gap-2 items-center
+        '>
+          <BiLogOut />
+          <p>Sign out</p>
+        </div>
       </section>
       {userType === 'EMPLOYER'
-        ? <section className='flex flex-col gap-2 px-4 py-2 text-lg borer-b-2 border-gray-300 md:hidden'>
+        ? <section className='flex flex-col gap-2 pl-4 py-2 text-lg md:hidden'>
           <p className='min-w-[84px] cursor-pointer md:hover:font-semibold duration-200'>My offers</p>
           <p className='min-w-[86px] cursor-pointer md:hover:font-semibold duration-200'>Post offer</p>
           <p className='min-w-[84px] cursor-pointer md:hover:font-semibold duration-200'>Favorites</p>
         </section>
-        : <section className='flex flex-col gap-2 px-4 py-2 text-lg borer-b-2 border-gray-300 md:hidden'>
-          <p className='min-w-[84px] cursor-pointer md:hover:font-semibold duration-200'>My candidatures</p>
-          <p className='min-w-[84px] cursor-pointer md:hover:font-semibold duration-200'>Favorites</p>
+        : <section className='flex flex-col gap-2 px-4 py-2 text-lg md:hidden justify-center'>
+          <div className='flex gap-2 items-center'>
+            <BsCardChecklist />
+            <p className='min-w-[84px] cursor-pointer md:hover:font-semibold duration-200'>My candidatures</p>
+          </div>
+          <div className='flex gap-2 items-center'>
+            <BsBookmarksFill />
+            <p className='min-w-[84px] cursor-pointer md:hover:font-semibold duration-200'>Favorites</p>
+          </div>
         </section>}
     </div>
   )
--- src/components/header/ProfileMenuButton.tsx
+++ src/components/header/ProfileMenuButton.tsx
@@ -1,14 +1,15 @@
+/* eslint-disable react/jsx-indent */
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable @typescript-eslint/promise-function-async */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 'use client'
 import Image from 'next/image'
 import { useState } from 'react'
-import { FaUserCircle } from 'react-icons/fa'
 import { HiOutlineMenu } from 'react-icons/hi'
 import MobileMenu from './MobileMenu'
 import { useAuth } from '@/hooks/useAuth'
-import { IoMdArrowDropdown } from 'react-icons/io'
+import Link from 'next/link'
+import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io'
 
 export default function AuthButton () {
   const [isOpen, setIsOpen] = useState(false)
@@ -16,12 +17,6 @@ export default function AuthButton () {
 
   const handleClick = () => {
     setIsOpen(prev => !prev)
-    // if (currentUser !== null) {
-    //   await logout()
-    //   router.push('/login')
-    // }
-
-    // router.push('/login')
   }
 
   return (
@@ -30,20 +25,22 @@ export default function AuthButton () {
         <div>
           {!isOpen && <HiOutlineMenu className='md:hidden' fontSize={28} />}
         </div>
-        {!userIsLoged() && <p className='text-lg font-medium'>Sign in</p>}
-        {userIsLoged() && currentUser?.photoURL !== ''
-          ? <>
-            <Image
-              src={currentUser?.photoURL as string}
-              alt={currentUser?.fullname as string + 'Profile Photo'}
-              width={40} height={40}
-              className='hidden md:block rounded-full object-cover w-[40px] h-[40px]'
-            />
-            <IoMdArrowDropdown className='hidden md:block' />
-          </>
-          : <FaUserCircle />}
+        {!userIsLoged() && <Link href='/login' className='text-lg font-medium hover:scale-105'>Sign in</Link>}
+        {userIsLoged() &&
+        <div className='flex items-center'>
+          <Image
+            width={60}
+            height={60}
+            className='hidden md:block rounded-full w-[40px] min-w-[40px] h-[40px] bg-white object-cover hover:scale-95'
+            src={currentUser?.photoURL !== '' ? currentUser?.photoURL as string : 'images/defaultProfilePhoto.svg'}
+            alt={currentUser?.fullname as string + 'profile photo' || 'Default profile photo'}
+          />
+          {isOpen
+            ? <IoMdArrowDropup className='hidden md:block hover:font-semibold' fontSize={28} />
+            : <IoMdArrowDropdown className='hidden md:block hover:font-semibold' fontSize={28} />}
+        </div>}
       </div>
-      <MobileMenu isOpen={isOpen} handleClick={handleClick} />
+      {userIsLoged() && <MobileMenu isOpen={isOpen} handleClick={handleClick} />}
     </>
   )
 }
